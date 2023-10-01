66427825243a1b9c7ba805f62c14e258e8f3dfe3
tri wahyudi <mesirah6@gmail.com>
2023-10-01 23:09:03 +0700

    add: live chat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,153 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="text-bg-light">
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col">
+                            Saldo
                         </div>
-                    @endif
+                        <div class="col">
+                            <i class="fa-solid fa-bell text-primary"></i>
+                        </div>
+                        <div class="col">
+                            <i class="fa-solid fa-cart-shopping text-primary"></i>
+                        </div>
+                    </div>
+                </div>
 
-                    {{ __('You are logged in!') }}
+                <div class="">
+                    <div class="">
+                        <div class="row bg-white border-4 p-3 m-0 rounded-4">
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/transfer" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Transfer.png') }}" alt="Transfer" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Transfer</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/tarik-tunai" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Tarik-tunai.png') }}" alt="Tarik-tunai" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Tarik Tunai</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/setor-tunai" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Setor-tunai.png') }}" alt="Setor-tunai" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Setor Tunai</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/bayar-cicilan" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Bayar.png') }}" alt="Bayar" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Bayar Cicilan</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/asuransi" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Asuransi.png') }}" alt="Asuransi" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Asuransi</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <div class="card h-100 border-0">
+                                    <div class="card-body text-center">
+                                        <img src="{{ asset('assets/images/Bank.png') }}" alt="Bank" class="img-fluid" style="filter:blur(3px);">
+                                        <h5 class="card-title pt-2 mb-0" style="font-size: 13px;">Pengajuan Pinjaman</h5>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/pulsa" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Pulsa.png') }}" alt="Pulsa" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Pulsa</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/pln" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Pln.png') }}" alt="Pln" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">PLN</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/topup" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Topup.png') }}" alt="Topup" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Top Up</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/dapur" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Dapur.png') }}" alt="Dapur" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Bahan Dapur</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-4 col-sm-6 col-md-4 col-lg-3 p-2">
+                                <a href="http://127.0.0.1:8000/sayur" class="text-decoration-none">
+                                    <div class="card h-100 border-0">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('assets/images/Sayur.png') }}" alt="Sayur" class="img-fluid">
+                                            <h5 class="card-title pt-2 mb-0" style="font-size: 14px;">Sayur Saji</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card h-100 border-0">
+                    <div class="card-body text-center">
+                        <div class="d-grid gap-2 pt-1">
+                            <a href="#" class="btn btn-primary btn-lg">Riwayat</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,6 +17,7 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -37,35 +39,34 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link bg-primary rounded-2 text-white text-center m-1" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link bg-primary rounded-2 text-white text-center m-1" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link bg-primary rounded-2 text-white text-center m-1" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link bg-primary rounded-2 text-white text-center m-1" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end bg-black" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item text-center text-white" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Keluar') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -78,5 +79,21 @@
     </div>
 
     <script src="https://kit.fontawesome.com/39d2ff4747.js" crossorigin="anonymous"></script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/64f36208b2d3e13950ed8b2a/1h9bbdt4i';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
+
 </html>
